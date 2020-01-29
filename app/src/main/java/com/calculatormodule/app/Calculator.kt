6dfacb75c2d68b9c74b4d0f0eb1f705426b8456b
professional-lalit <lalit.appsmail@gmail.com>
2020-01-30 00:44:40 +0530

    initial commit to library

---
+++ app/src/main/java/com/calculatormodule/app/Calculator.kt
@@ -0,0 +1,21 @@
+package com.calculatormodule.app
+
+class Calculator {
+
+    fun add(a: Int, b: Int):Int{
+        return a+b
+    }
+
+    fun sub(a: Int, b: Int):Int{
+        return a-b
+    }
+
+    fun mul(a: Int, b: Int):Int{
+        return a*b
+    }
+
+    fun div(a: Int, b: Int):Int{
+        return a/b
+    }
+
+}
